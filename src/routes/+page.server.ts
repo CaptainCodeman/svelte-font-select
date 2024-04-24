385342b9e8cb1ee53c2eceac5b36edf18177dd7a
Simon Green <simon@captaincodeman.com>
2024-04-24 13:07:51 -0600

    try static data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,10 @@
-import { GOOGLE_FONTS_API_KEY } from '$env/static/private'
-import { createServerLoad } from 'svelte-font-select'
+// import { GOOGLE_FONTS_API_KEY } from '$env/static/private'
+// import { createServerLoad } from 'svelte-font-select'
 
-export const load = createServerLoad(GOOGLE_FONTS_API_KEY)
+// export const load = createServerLoad(GOOGLE_FONTS_API_KEY)
+
+import data from '../../static/fonts.json'
+
+export async function load({ fetch }) {
+	return data
+}
